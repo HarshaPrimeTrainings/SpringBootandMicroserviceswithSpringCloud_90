39c6259255604601c7c08223ce06c03361d11ab4
HarshaVardhan <athaluri.harsha@gmail.com>
2026-07-03 08:07:25 +0530

    onetoone foregnkey sutomization

---
+++ OneToOneMapping_custom_forign_key/userservice/src/main/java/com/training/userservice/dao/User.java
@@ -0,0 +1,69 @@
+package com.training.userservice.dao;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer uid;
+	private String name;
+	private String contact;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "addr_zip",referencedColumnName = "zipcode")
+	private Address addr;
+
+	
+	
+	private String passwrod;
+	
+
+	
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	
+	
+	public String getPasswrod() {
+		return passwrod;
+	}
+
+	public void setPasswrod(String passwrod) {
+		this.passwrod = passwrod;
+	}
+	public Address getAddr() {
+		return addr;
+	}
+	public void setAddr(Address addr) {
+		this.addr = addr;
+	}
+	
+}
