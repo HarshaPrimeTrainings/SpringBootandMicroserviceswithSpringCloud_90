6c72af9ff5a29506d416260810570248ded52e8a
HarshaVardhan <athaluri.harsha@gmail.com>
2026-07-07 12:44:57 +0530

    many to many

---
+++ ManyToMany/userservice/src/main/java/com/training/userservice/dao/User.java
@@ -0,0 +1,82 @@
+package com.training.userservice.dao;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer uid;
+	private String name;
+	
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "user")
+	private List<Contact> contact;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "addr_zip",referencedColumnName = "zipcode")
+	private Address addr;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+//	@JoinTable(name = "user_cource",joinColumns = @JoinColumn(name="user_id"),
+//	inverseJoinColumns = @JoinColumn(name="cource_id"))
+	private List<Cource> cources;
+
+	private String passwrod;
+	
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Contact> getContact() {
+		return contact;
+	}
+	public void setContact(List<Contact> contact) {
+		this.contact = contact;
+	}
+	public String getPasswrod() {
+		return passwrod;
+	}
+
+	public void setPasswrod(String passwrod) {
+		this.passwrod = passwrod;
+	}
+	public Address getAddr() {
+		return addr;
+	}
+	public void setAddr(Address addr) {
+		this.addr = addr;
+	}
+	public List<Cource> getCources() {
+		return cources;
+	}
+	public void setCources(List<Cource> cources) {
+		this.cources = cources;
+	}
+	
+}
